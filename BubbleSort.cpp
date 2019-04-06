1dbac2983b2ea027f1289bdceb9288902944b71c
Tanukiman <38245109+Tanukiman@users.noreply.github.com>
2019-04-06 21:17:38 +0900

    add BubbleSort.cpp file via upload

---
+++ BubbleSort.cpp
@@ -0,0 +1,16 @@
+void swap (int *a, int *b){
+    int tmp = *b;
+    *b = *a;
+    *a = tmp;
+}
+
+void BubbleSort (int* first, int* last){
+    int size = last - first;
+    for(int i=0; i<size; ++i){
+        for(int j=1; j<size-i; ++j){
+           if(first[j] < first[j-1]){
+              swap(&first[j-1], &first[j]);
+            }
+        }
+    }
+}
